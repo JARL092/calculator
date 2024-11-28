93d521c35244e4b059a0f012de3027ffd1fb8016
JARL092 <106961602+JARL092@users.noreply.github.com>
2024-11-28 13:17:36 +0200

    Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,42 +3,15 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Добро пожаловать в калькулятор!");
-        System.out.print("Введите первое число: ");
-        double num1 = scanner.nextDouble();
-
-        System.out.print("Введите операцию (+, -, *, /): ");
-        char operator = scanner.next().charAt(0);
-
-        System.out.print("Введите второе число: ");
-        double num2 = scanner.nextDouble();
-
-        double result;
-
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Ошибка: деление на ноль!");
-                    return;
-                }
-                break;
-            default:
-                System.out.println("Ошибка: недопустимая операция!");
-                return;
-        }
-
-        System.out.println("Результат: " + result);
+        
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        
+        int sum = a + b;
+        
+        System.out.println("Sum: " + sum);
     }
-}  
+}
